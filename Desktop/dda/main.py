7ecfa7bb849ca188488eb44605fe32f6e49cfb1b
Gangula-Varshitha <96340672+Gangula-Varshitha@users.noreply.github.com>
2022-01-09 23:18:31 +0530

    Delete Desktop/dda directory

--- Desktop/dda/main.py
+++
@@ -1,8 +0,0 @@
-import demographic
-from unittest import main
-
-# Test your function by calling it here
-demographic.calculate_demographic_data()
-
-# Run unit tests automatically
-main(module='test_module', exit=False)
