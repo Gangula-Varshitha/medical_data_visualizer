798da544477c6e9d725915a2c855af8783b5ccb7
Gangula-Varshitha <varshitha817@gmail.com>
2022-01-09 23:00:04 +0530

    mddv

---
+++ Desktop/dda/main.py
@@ -0,0 +1,8 @@
+import demographic
+from unittest import main
+
+# Test your function by calling it here
+demographic.calculate_demographic_data()
+
+# Run unit tests automatically
+main(module='test_module', exit=False)
